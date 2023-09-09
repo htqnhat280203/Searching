a106df569eb91281cec9f3ffcb0af0193d5a2c76
htqnhat280203 <144199668+htqnhat280203@users.noreply.github.com>
2023-09-09 11:10:44 +0700

    Add files via upload

---
+++ src/common/Algorithm.java
@@ -0,0 +1,59 @@
+package Common;
+
+public class Algorithm {
+    
+    public void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+    
+    public int binarySearch(int[] array, int value, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+        int middle = (left + right) / 2;
+        if (array[middle] == value) {
+            return middle;
+        } else if (array[middle] > value) {
+            return binarySearch(array, value, left, middle - 1);
+        } else {
+            return binarySearch(array, value, middle + 1, right);
+        }
+    }
+    public int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Trả về chỉ số nếu tìm thấy giá trị
+            }
+        }
+        return -1; // Trả về -1 nếu không tìm thấy giá trị
+    }
+    
+
+    
+//    public int binarySearch(int[] arr, int target) {
+//        int left = 0;
+//        int right = arr.length - 1;
+//
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//
+//            if (arr[mid] == target) {
+//                return mid;
+//            } else if (arr[mid] < target) {
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//        }
+//
+//        return -1; // target not found
+//    }
+}
